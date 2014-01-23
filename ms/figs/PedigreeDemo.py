33dde817cd6827e189f220e493e30f9be7ec0a35
gw <gw@493babdc-ab97-468b-a2ee-949ea25b5092>
2014-01-23 22:40:56 +0000

    add ms for methods

---
+++ ms/figs/PedigreeDemo.py
@@ -0,0 +1,102 @@
+import os, sys
+from subprocess import Popen, PIPE
+
+def plotdemo(title, genotype = None):
+  rdat = '''
+    suppressMessages(library('kinship2'))
+    data(sample.ped)'''
+  docs = '''
+    ## fam1 <- sample.ped[sample.ped$ped==1,]
+    ## ped1 <- pedigree(fam1$id, fam1$father, fam1$mother,
+    ## fam1$sex, fam1$affected, fam1$avail)
+    ## ## to see plot:
+    ## plot.pedigree(ped1, align=FALSE)
+    ## id1 <- pedigree.unrelated(ped1, avail=fam1$avail)
+    ## id1
+    ## ## some possible vectors
+    ## ##[1] "110" "113" "133" "109"
+    ## ##[1] "113" "118" "141" "109"
+    ## ##[1] "113" "118" "140" "109"
+    ## ##[1] "110" "113" "116" "109"
+    ## ##[1] "113" "133" "141" "109"
+    ## fam2 <- sample.ped[sample.ped$ped==2,]
+    ## ped2 <- pedigree(fam2$id, fam2$father, fam2$mother,
+    ## fam2$sex, fam2$affected, fam2$avail)
+    ## ## to see plot:
+    ## ## plot.pedigree(ped2, align=FALSE)
+    ## id2 <- pedigree.unrelated(ped2, avail=fam2$avail)
+    ##'''
+  fam1 = '''
+    dat <- sample.ped[sample.ped$ped==2,]
+    '''
+  nuc1 = '''
+    dat <- sample.ped[sample.ped$ped==2,]
+    dat <- dat[dat$id %in% c(201,202,205,206,207),]
+    dat$affected[2] <- 0
+    dat$avail <- 0
+    '''
+  prt = '''
+    dat$father[which(dat$father == 0)] <- NA
+    dat$mother[which(dat$mother == 0)] <- NA
+    dat <- pedigree(as.character(dat$id), as.character(dat$father), as.character(dat$mother),
+    dat$sex, dat$affected, dat$avail)
+    png('{0}.png')
+    plot.pedigree(dat, symbolsize = 1, cex = 1.5, align = T)
+    dev.off()
+    '''.format(title)
+  if genotype:
+    edit = '''
+    genotype <- %s
+    if (length(genotype) > length(dat$id)) {
+    genotype <- genotype[c(1,length(dat$id))]
+    } else {
+    # genotype <- c(genotype, dat$id[c(length(genotype)+1, length(dat$id))])
+    genotype <- c(genotype, rep(NULL, length(dat$id) - length(genotype)))
+    }
+    id <- dat$id
+    #
+    for (i in 1:length(dat$id))
+    dat$id[i] <- ifelse(is.null(genotype[i]), dat$id[i], paste(dat$id[i], genotype[i], sep='%s'))
+    #
+    for (i in 1:length(dat$father)) {
+    for (j in 1:length(id)) {
+    if (dat$father[i] == id[j]) {
+    dat$father[i] = dat$id[j]
+    break
+    }
+    }
+    }
+    #
+    for (i in 1:length(dat$mother)) {
+    for (j in 1:length(id)) {
+    if (dat$mother[i] == id[j]) {
+    dat$mother[i] = dat$id[j]
+    break
+    }
+    }
+    }
+    ''' % ('c({})'.format(', '.join(['"{}"'.format('\n'.join(i)) for i in genotype])), '\n')
+  else:
+    edit = ''
+  return rdat + nuc1 + edit + prt
+
+def run(genotype, title):
+  Popen(["R", "--slave"], stdin=PIPE).communicate(plotdemo(title, genotype))
+
+def montage(title, tile):
+  os.system("montage *.*.png -geometry 960x960+1+1 -tile %s %s" % (tile, title))
+  os.system("rm *.*.png")
+  
+if __name__ == "__main__":
+    if len(sys.argv) < 2:
+        sys.exit()
+    if sys.argv[1] == '1':
+        run([('AC','0?','0?'),('BD','0?','0?'),('--','??','00'),('--','00','??'),('AB','00','00')], '1.1')
+        run([('AC','00','01'),('BD','01','00'),('--','??','00'),('--','00','??'),('AB','00','00')], '1.2')
+        run([('AC','00','01'), ('BD','01','00'), ('AD','01','00'), ('BC','00','01'), ('AB','00','00')], '1.3')
+        run([('12',), ('13',), ('13',), ('12',), ('11',)], '1.4')
+        run([('AC','00','01'), ('BD','01','00'), ('DA','10','00'), ('BC','00','01'), ('AB','00','00')], '1.5')
+        run([('12',), ('13',), ('31',), ('12',), ('11',)], '1.6')
+        montage('Figure1.png', '2x3')
+    if sys.argv[1] == '2':
+        pass
