0fe1ab7521c7e552f5f138a3223d5cbf9be76614
gw <gw@493babdc-ab97-468b-a2ee-949ea25b5092>
2014-03-31 20:26:24 +0000

    update setup.py

--- package/setup.py
+++ package/setup.py
@@ -4,17 +4,36 @@
 # Copyright (c) 2014, Gao Wang <ewanggao@gmail.com>
 # GNU General Public License (http://www.gnu.org/licenses/gpl.html)
 
-import sys, imp
-try:
-    imp.find_module('cstatgen')
-except ImportError:
-    sys.exit('''Please install Gao Wang's "cstatgen" library''')
-
+import sys, imp, os
 for item in ['faulthandler', 'numpy', 'matplotlib', 'prettyplotlib']:
     try:
         imp.find_module(item)
     except ImportError:
         sys.exit('Cannot build package: missing module "{}"!'.format(item))
+
+try:
+    imp.find_module('cstatgen')
+except ImportError:
+    from source.Utils import downloadURL
+    from distutils.dir_util import remove_tree
+    import tempfile
+    cstatgen_url = "http://tigerwang.org/uploads/cstatgen.tar.gz"
+    download_dir = tempfile.gettempdir()
+    pkg = os.path.join(download_dir, "cstatgen.tar.gz")
+    pkg_dir = os.path.join(download_dir, "cstatgen")
+    sys.stderr.write("Downloading cstatgen library ...\n")
+    downloadURL(cstatgen_url, download_dir, force = True)
+    sys.stderr.write("Installing cstatgen library ...\n")
+    try:
+        remove_tree(pkg_dir)
+    except:
+        pass
+    os.system("tar zxvf {} -C {} > /dev/null".format(pkg, download_dir))
+    cwd = os.getcwd()
+    os.chdir(pkg_dir)
+    cmd = "python setup.py install {}".format(" ".join(sys.argv[2:]))
+    os.system("{} > /dev/null".format(cmd))
+    os.chdir(cwd)
 
 from distutils.core import setup
 try:
@@ -26,9 +45,6 @@ from source import VERSION
 
 NAME = "SEQLinco"
 
-if sys.platform == "win32":
-    sys.exit('Windows OS is not supported.')
-    
 setup(name = NAME,
     version = VERSION,
     description = "A novel approach to use sequence data for linkage analysis",
