2b265d5b82b10bc79160e3c6dd9d45d1307c2b73
zhangdi <zhangdi@493babdc-ab97-468b-a2ee-949ea25b5092>
2014-11-04 04:41:51 +0000

    RunGeneHunter.py ready

--- simulations/RunGeneHunter.py
+++ simulations/RunGeneHunter.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python
 
-import sys, os, argparse, tabix, subprocess
+import sys, os, argparse, gzip, subprocess
 
 ##Global conf of each running instance.
 ##These are just some naming conventions of files and the genes to be
@@ -10,7 +10,7 @@ class Conf:
     def __init__(self, args):
         self.args = args
         ##path and program conventions
-        self.srcDir = '../0data'
+        self.srcDir = '../0data/rep{}'.format(args.rep)
         self.tgtDir = 'rep{}'.format(args.rep)
         self.Program = 'gh'
         self.srcFile = 'FAM{}RUN{}rep{}.{{}}'.format(args.fam, args.run, args.rep)
@@ -24,21 +24,20 @@ class Conf:
         self.moi = 'AR'
         self.moiPrev = {'AR': '0.001 0.001 0.999',
                         'AD': '0.001 0.999 0.999'}
-        self.markAF = '0.01'
-        self.markRR = '0.00001'
+        self.markAF = 0.01
+        self.markRR = 0.00001
         self.locTpl = self.getLocTemplate()
         ##Genehunter parameters
         self.step = 1
         self.mapFunc = 'haldane'
         
-
     def getLocTemplate(self):
         head = '{} 0 0 5\n0 0.0 0.0 0\n'
-        loci = '{}\n'
+        loci = '{}\n1 2\n0.99 0.01\n'
         disloc = '1\n{}\n'.format(self.moiPrev[self.moi])
         markers = '{}'
         sex = '0 0\n'
-        recRate = '0.0{}\n'
+        recRate = '0.0 {}\n'
         recInc = '1 0.05 0.45\n'
         Tpl = head + loci + disloc + markers + sex + recRate + recInc
         return Tpl
@@ -46,8 +45,9 @@ class Conf:
 
 ##Utils
 #Unfortunately, python lacks the arbitary array slice
-def getSlice(idxes, lst):
+def getSlice(lst, idxes):
     res = []
+    #print idxes
     for i in idxes:
         res.append(lst[i])
     return res
@@ -60,6 +60,7 @@ def getSrc(cnf, ext):
 #get target file name by ext and gene No.
 def getTgt(cnf, ext, gene):
     tgt = os.path.join(cnf.tgtDir, cnf.tgtFile.format(gene, ext))
+    return tgt
 
 #cd
 class cd:
@@ -86,7 +87,7 @@ class Loc:
         self.numMarkers = num
        
     def getLoci(self):
-        return ' '.join(map(lambda x: 1 + x, range(self.numLoci)))
+        return ' '.join(map(lambda x: str(1 + x), range(self.numLoci)))
     
     def getMarkers(self):
         res = ''
@@ -95,9 +96,10 @@ class Loc:
         return res
         
     def getRates(self):
-        return ' '.join([self.markRR] * (self.numMarkers - 1))
+        return ' '.join([str(self.markRR)] * (self.numMarkers - 1))
 
     def make(self):
+        #print self.File
         with open(self.File, 'w') as l:
             l.write(self.tpl.format(self.numLoci, self.getLoci(), self.getMarkers(), self.getRates()))
  
@@ -108,13 +110,13 @@ class Pre:
         self.offset = cnf.PedOffset
         self.idxes = idxes
 
-    def make(self, offset):
+    def make(self):
         with open(self.ped) as p, open(self.File, 'w') as f:
             for line in p:
                 s = line.strip('\n').split()
-                fam = s[:offset]
-                geno = getSlice(s[offset:], self.idxes)
-                f.write('{} {}\n'.format(fam, geno))
+                fam = map(lambda x: x.split(':')[-1], s[:self.offset])
+                geno = getSlice(s[self.offset:], self.idxes)
+                f.write('{} {}\n'.format(' '.join(fam), ' '.join(geno)))
 
 ##Meaningful funtions
 #parse the vcf to determine the non-all-wildtype sites of a gene
@@ -122,28 +124,61 @@ def FindSites(cnf, gene):
     pos = cnf.Chr[gene]
     vcf = getSrc(cnf, 'vcf.gz')
     res = []
-    with tabix.open(vcf) as v:
-        for idx,l in enumerate(v.query(pos)):
-            g = l.strip('\n').split()[9:]
+    idx = 0
+    with gzip.open(vcf) as v:
+        for l in v:
+            if l.startswith('#'):
+                continue
+            s = l.strip('\n').split()
+            if s[0] != str(pos):
+                idx += 1
+                continue
+            g = s[9:]
             if len(filter(lambda x: x != '0/0', g)) > 0:
-                res.append(idx * 2 + cnf.PedOffset)
-                res.append(idx * 2 + cnf.PedOffset + 1)
+                res.append(idx * 2)
+                res.append(idx * 2 + 1)
+            idx += 1
+    #print res
     return res
 
 #prepare file for genehunter, most of the job was done by the Loc and
 #Pre classes.
 def PrepFiles(cnf, idxes, gene):
-    l = Loc(cnf, len(idxes), gene).make()
-    p = Pre(cnf, idxes, gene).make()
+    l = Loc(cnf, len(idxes)/2, gene)
+    l.make()
+    p = Pre(cnf, idxes, gene)
+    p.make()
     return (l, p)
 
+##Find the highest lod score and store it in a file
+def FindLod(res, lods):
+    start, end, val = (0, 0, -100)
+    with open(lods, 'w') as o:
+        for idx, line in enumerate(res.split('\n')):
+            #print idx
+            #print '{} {} {}'.format(start, end, line[:6])
+            if line[:6] == 'npl:7>':
+                o.write('{}\n'.format(val))
+                break
+            if start == 1:
+                if line.strip() != '':
+                    lod = line.strip().split()[1]                    
+                    if lod > val:
+                        val = lod
+            if line[:6] == 'npl:6>':
+                start = 1
+                #print start
+ 
 #Actually run the genehunter
 def RunGeneHunter(Loc, Pre, cnf):
-    with cd(Dir):
+    with cd(cnf.tgtDir):
         cmds = 'load {}\nincrement steps {}\nmap function {}\nanalysis lod\nscan {}\ntotal stat\n'
-        cmds = cmds.format(Loc.File, cnf.step, cnf.mapFunc, Pre.File)
+        cmds = cmds.format(os.path.basename(Loc.File), cnf.step, cnf.mapFunc, os.path.basename(Pre.File))
+        #print cmds
         gh = subprocess.Popen('gh', stdin=subprocess.PIPE, stdout=subprocess.PIPE)
-        gh.communicate(cmds)
+        res = gh.communicate(cmds)[0]
+        lods = os.path.splitext(os.path.basename(Loc.File))[0] + '.lods'
+        FindLod(res, lods)
         try:
             gh.kill()
         except:
@@ -162,7 +197,7 @@ def main():
     for gene in cnf.genes:
         Idxes = FindSites(cnf, gene)
         Loc, Pre = PrepFiles(cnf, Idxes, gene)
-        RunGeneHunter(Loc, Pre, cnf.tgtDir)
+        RunGeneHunter(Loc, Pre, cnf)
 
 
 if __name__ == '__main__':
