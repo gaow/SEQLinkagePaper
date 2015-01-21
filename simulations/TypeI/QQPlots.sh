d1599a78d07a27fd2e35149f6593eb86b93a9ba7
Gao Wang Office <gaow@bcm.edu>
2015-01-21 16:26:04 -0600

    polish qq plot script

--- simulations/TypeI/QQPlots.sh
+++ simulations/TypeI/QQPlots.sh
@@ -3,18 +3,62 @@ grep $gene $null | cut -f2 > /tmp/1.txt
 grep $gene $recomb | cut -f2 > /tmp/2.txt
 grep $gene $ldmissing | cut -f2 > /tmp/3.txt
 echo """ 
-d1 = sort(scan('/tmp/1.txt'))
+library('ggplot2')
+gghist <- function(dat, stat_foo = NULL, vlines = NULL, xname='x', verbose = F) {
+        average <- round(mean(dat[,xname], na.rm=T),4)
+        stdev <- round(sd(dat[,xname], na.rm=T),4)
+        #med <- round(median(dat[,xname], na.rm=T), 4)
+        r1 <- round(min(dat[,xname], na.rm=T),4)
+        r2 <- round(max(dat[,xname], na.rm=T),4)
+        # convert dat obj from numeric to data.frame
+        myplot <- ggplot(dat) +
+                        aes_string(x = xname) +
+                        geom_histogram(aes_string(y = '..density..', fill = '..density..'), color = 'white', binwidth = (r2-r1)/30) +
+                        scale_fill_gradient('bin mass', low = 'darkolivegreen3', high = colors()[552]) +
+                        geom_rug(color = 'grey80') +
+                        scale_x_continuous(name = paste('\\n', xname)) +
+                        scale_y_continuous(name = 'Density\\n') +
+                        theme_bw()
+        if (!is.null(vlines)) myplot <- myplot + geom_vline(xintercept = vlines, color = '#9ACD32', linetype = 2)
+        if (!is.null(stat_foo)) {
+        myplot <- myplot + geom_line(stat = 'density', size = 0.5, linetype = 2, color = 'grey50') + stat_function(fun = stat_foo, color = 'red')
+        plottitle <- 'Histogram & fitted vs. normal distribution density curve for '
+        } else {
+        plottitle <- 'Histogram for '
+        }
+        if (verbose) {
+        myplot <- myplot + labs(title = paste(plottitle, xname, '\\n', 'mean = ', toString(average), '; ', 'stdev = ', toString(stdev), '; ', 'range = ', '[', toString(r1), ',', toString(r2), ']', '\\n', sep=''))
+        } else {
+        myplot <- myplot + labs(title = paste(plottitle, xname, '\\n', sep=''))
+        }
+        return(myplot)
+}
+ggdot <- function(dat, psize=2.5, color=NULL, xyz = NULL, title = '') {
+    if (!is.null(xyz)) {
+    colnames(dat) = xyz
+    } else {
+    xyz = colnames(dat)
+    }
+    myplot = ggplot(dat, aes_string(xyz[1], xyz[2])) +
+        geom_point(size = psize, binwidth = range(dat[,1])/30) +
+        xlab(paste('\\n', xyz[1], sep='')) + ylab(paste(xyz[2], '\\n', sep='')) +
+        geom_abline(intercept = 0, slope = 1) +
+	    theme(axis.title.x=element_text(size=10)) +
+	    theme(axis.title.y=element_text(size=10)) +
+	    theme_bw() +
+        labs(title=paste(title,'\\n'))
+    return(myplot) 
+}
+HLOD = sort(scan('/tmp/1.txt'))
 d2 = sort(scan('/tmp/2.txt'))
 d3 = sort(scan('/tmp/3.txt'))
 d20 = sort(scan('/tmp/1.txt')[1:length(d2)])
 d30 = sort(scan('/tmp/1.txt')[1:length(d3)])
-print(length(which(d1>3.6)))
+print(length(which(HLOD>3.6)))
 pdf('$gene.pdf')
-hist(d1)
-plot(d20,d2)
-abline(0,1)
-plot(d30,d3)
-abline(0,1)
+print(gghist(data.frame(HLOD), xname='HLOD', vlines = c(3.6)))
+print(ggdot(data.frame(d20,d2), xyz = c('Expected','Observed'), title = 'HLOD null distribution in the presence of recombination events'))
+print(ggdot(data.frame(d30,d3), xyz = c('Expected','Observed'), title = 'HLOD null distribution in the presence of LD and missing data'))
 dev.off()
 """ | R --slave  
 if [ "$(uname)" == "Darwin" ]; then
