124d3574547ea410fce5e30a8b3eebdc2e046181
Gao Wang Office <gaow@bcm.edu>
2015-02-06 12:25:13 -0600

    Add test script for Missing data case; data file pending

--- package/test/Interface/test.sh
+++
@@ -1,4 +0,0 @@
-seqlink --vcf test.vcf.gz --fam test.tfam -j8 --freq AF -o InputAF \
-    --prevalence 0.01 --moi AD -W 0 -M 1 --run-linkage
-seqlink --vcf test.vcf.gz --fam test.tfam -j8 -o SampleAF \
-    --prevalence 0.01 --moi AD -W 0 -M 1 --run-linkage
